3104c6b5c39266fe3ecb35d28164896186ad5f60
mjschock <mschock@ocf.berkeley.edu>
2015-10-11 12:33:14 -0700

    fixing linearRegression; WIP

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ function validate(options) {
     if (options.y instanceof Array === false) {
         throw 'y must be an Array';
     }
+    if (!options.alpha) {
+        throw 'alpha is required';
+    }
+    if (Number.isNaN(Number.parseFloat(options.alpha))) {
+        throw 'alpha must be a number';
+    }
+    if (!options.iterations) {
+        throw 'iterations is required';
+    }
+    if (!Number.isInteger(options.iterations)) {
+        throw 'iterations must be an integer';
+    }
 }
 
 exports.linearRegression = function(options) {
@@ -24,24 +36,45 @@ exports.linearRegression = function(options) {
             validate(options);
             var x = math.matrix(options.x),
                 y = math.matrix(options.y),
-                m = x.size()[1],
+                // m = x.size()[1],
                 dimx = x.size(),
                 featureCount = dimx[1],
                 trainingCount = dimx[0],
                 alpha = options.alpha,
                 iterations = options.iterations,
-                theta = math.zeros(featureCount),
+                theta = math.zeros(featureCount, 1),
                 k = (alpha / trainingCount);
+
+            console.log('x: ', x);
+            console.log('y: ', y);
+            // console.log('m: ', m);
+            console.log('dimx: ', dimx);
+            console.log('featureCount: ', featureCount);
+            console.log('trainingCount: ', trainingCount);
+            console.log('alpha: ', alpha);
+            console.log('iterations: ', iterations);
+            console.log('theta: ', theta);
+            console.log('k: ', k);
 
             for (var i = 0; i < iterations; i++) {
                 var temp = theta.clone();
+                console.log('temp: ', temp);
 
                 for (var j = 0; j < featureCount; j++) {
-                    var hypothesis = math.multiply(x, theta),
-                        error = math.subtract(hypothesis, y),
-                        xi = math.subset(x, math.index([0, trainingCount], j)),
-                        sum = math.multiply(error.transpose(), xi),
-                        newTheta = theta.subset(math.index(j)) - k * sum;
+                    var hypothesis = math.multiply(x, theta);
+                    console.log('hypothesis: ', hypothesis);
+                    console.log('hypothesis size: ', hypothesis.size());
+                    console.log('y size: ', y.size());
+                    console.log('theta size: ', theta.size());
+                    var error = math.subtract(hypothesis, y);
+                    console.log('error: ', error);
+                    console.log('math.index([0, trainingCount], j): ', math.index([0, trainingCount], j));
+                    var xi = math.subset(x, math.index([0, trainingCount], j));
+                    console.log('xi: ', xi);
+                    var sum = math.multiply(error.transpose(), xi);
+                    console.log('sum: ', sum);
+                    var newTheta = theta.subset(math.index(j)) - k * sum;
+                    console.log('newTheta: ', newTheta);
 
                     if (newTheta === Number.POSITIVE_INFINITY || newTheta === Number.NEGATIVE_INFINITY) {
                         reject('theta has diverged! - try another alpha');
--- spec/jasmine_specs/indexSpec.js
+++ spec/jasmine_specs/indexSpec.js
@@ -51,5 +51,71 @@ describe('index', function() {
                 done();
             });
         });
+
+        it('returns an error if options.alpha is not provided', function(done) {
+            linearRegression({
+                x: [],
+                y: []
+            }).catch(function(message) {
+                expect(message).toBe("alpha is required");
+                done();
+            });
+        });
+
+        it('returns an error if options.alpha is not a number', function(done) {
+            linearRegression({
+                x: [],
+                y: [],
+                alpha: 'alpha is not a number'
+            }).catch(function(message) {
+                expect(message).toBe("alpha must be a number");
+                done();
+            });
+        });
+
+        it('returns an error if options.iterations is required', function(done) {
+            linearRegression({
+                x: [],
+                y: [],
+                alpha: 0.001
+            }).catch(function(message) {
+                expect(message).toBe("iterations is required");
+                done();
+            });
+        });
+
+        it('returns an error if options.iterations is not an integer', function(done) {
+            linearRegression({
+                x: [],
+                y: [],
+                alpha: 0.001,
+                iterations: 0.001
+            }).catch(function(message) {
+                expect(message).toBe("iterations must be an integer");
+                done();
+            });
+        });
+
+        xit('returns an error if options.iterations is not an integer', function(done) {
+            linearRegression({
+                x: [],
+                y: [],
+                alpha: 0.001,
+                iterations: 1500
+            }).catch(function(message) {
+                expect(message).toBe("iterations must be an integer");
+                done();
+            });
+        });
+
+        it('returns an error if options.iterations is not an integer', function(done) {
+            var data = require('./data.json');
+            console.log('iterations: ', data);
+            linearRegression(data).catch(function(message) {
+                expect(message).toBe("iterations must be an integer");
+                done();
+            });
+        });
+
     });
 });
