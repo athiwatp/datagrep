59235d82ae4d984856e653114611f0af32951e4c
mjschock <mschock@ocf.berkeley.edu>
2016-12-18 12:15:29 -0800

    export default object

--- spec/computeCost.spec.js
+++ spec/computeCost.spec.js
@@ -1,15 +1,15 @@
-import computeCost from '../src/app/computeCost';
+import datagrep from '../src/index';
 import { parseCsv } from '../src/utils/csv';
 import { getNullMatrix, numCols, parseVariableMatrices } from '../src/utils/linearAlgebra';
 import text from './ex1data1.txt';
 
-describe("computeCost", () => {
+describe("datagrep.computeCost", () => {
 
   it("computes the cost", async (done) => {
       let data = await parseCsv(text);
       let { X, y } = parseVariableMatrices(data);
       let theta = getNullMatrix(1, numCols(X));
-      let cost = computeCost(X, y, theta);
+      let cost = datagrep.computeCost(X, y, theta);
 
       expect(Number.parseFloat(cost.toFixed(4))).toBe(32.0727);
       done();
--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import computeCost from './app/computeCost';
 export {
     computeCost
 }
+
+export default {
+    computeCost
+}
